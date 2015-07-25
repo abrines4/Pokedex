29aad6b02280e9a3e963024e89159c0ef7850bf5
Landon Prewitt <ltp368@build.tamu.edu>
2015-07-25 18:18:43 -0500

    gym.sql added

---
+++ gym.sql
@@ -0,0 +1,72 @@
+/* 
+Attributes:
+	gymName VARCHAR(20) PRIMARY KEY,
+	leaderName VARCHAR(20),
+	gymType VARCHAR(20),
+	gymBadge VARCHAR(20)
+*/
+
+INSERT INTO Gym VALUES ('Pewter','Brock','Rock','Boulder');
+INSERT INTO Gym VALUES ('Cerulean','Misty','Water','Cascade');
+INSERT INTO Gym VALUES ('Vermilion','Lt. Surge','Grass','Thunder');
+INSERT INTO Gym VALUES ('Celadon','Erika','Grass','Rainbow');
+INSERT INTO Gym VALUES ('Fuchisia','Koga','Poison','soul');
+INSERT INTO Gym VALUES ('Saffron','Sabrina','Psychic','Marsh');
+INSERT INTO Gym VALUES ('Cinnabar','Blaine','Fire','Volcano');
+INSERT INTO Gym VALUES ('Viridian','Giovanni','Ground','Earth');
+INSERT INTO Gym VALUES ('Violet','Falkner','Flying','Zephyr');
+INSERT INTO Gym VALUES ('Azalea','Bugsy','Bug','Hive');
+INSERT INTO Gym VALUES ('Goldenrod','Whitney','Normal','Plain');
+INSERT INTO Gym VALUES ('Ecruteak','Morty','Ghost','Fog');
+INSERT INTO Gym VALUES ('Cianwood','Chuck','Fighting','Storm');
+INSERT INTO Gym VALUES ('Olivine','Jasmine','Steel','Mineral');
+INSERT INTO Gym VALUES ('Mahogany','Pryce','Ice','Glacier');
+INSERT INTO Gym VALUES ('Blckthorn','Clair','Dragon','Rising');
+INSERT INTO Gym VALUES ('Rustboro','Roxanne','Rock','Stone');
+INSERT INTO Gym VALUES ('Dewford','Brawly','Fighting','Knuckle');
+INSERT INTO Gym VALUES ('Mauville','Wattson','Electric','Dynamo');
+INSERT INTO Gym VALUES ('Petalburg','Norman','Normal','Balance');
+INSERT INTO Gym VALUES ('Fortree','Winona','Flying','Feather');
+INSERT INTO Gym VALUES ('Mossdeep','Tate and Liza','Psychic','Mind');
+INSERT INTO Gym VALUES ('Sootopolis','Wallace','Water','Rain');
+INSERT INTO Gym VALUES ('Oreburgh','Roark','Rock','Coal');
+INSERT INTO Gym VALUES ('Eterna','Gardenia','Grass','Forest');
+INSERT INTO Gym VALUES ('Veilstone','Maylene','Fighting','Cobble');
+INSERT INTO Gym VALUES ('Pastoria','Crasher Wake','Water','Fen');
+INSERT INTO Gym VALUES ('Hearthome','Fantina','Ghost','Relic');
+INSERT INTO Gym VALUES ('Canalave','Bryon','Steel','Mine');
+INSERT INTO Gym VALUES ('Snowpoint','Candice','Ice','Icicle');
+INSERT INTO Gym VALUES ('Sunnyshore','Volkner','Electric','Beacon');
+INSERT INTO Gym VALUES ('Striaton','Cilan','Grass','Trio');
+INSERT INTO Gym VALUES ('Striaton','Chili','Fire','Trio');
+INSERT INTO Gym VALUES ('Striaton','Cress','Water','Trio');
+INSERT INTO Gym VALUES ('Nacrene','Lenora','Normal','Basic');
+INSERT INTO Gym VALUES ('Castelia','Burgh','Bug','Insect');
+INSERT INTO Gym VALUES ('Nimbasa','Elesa','Electric','Bolt');
+INSERT INTO Gym VALUES ('Driftveil','Clay','Ground','Quake');
+INSERT INTO Gym VALUES ('Mistralton','Skyla','Flying','Jet');
+INSERT INTO Gym VALUES ('Icirrus','Brycen','Ice','Freeze');
+INSERT INTO Gym VALUES ('Opelucid','Drayden','Dragon','Legend');
+INSERT INTO Gym VALUES ('Aspertia','Cheren','Normal','Basic');
+INSERT INTO Gym VALUES ('Virbank','Roxie','Poison','Toxic');
+INSERT INTO Gym VALUES ('Humilau','Marlon','Water','Wave');
+INSERT INTO Gym VALUES ('Santalune','Viola','Bug','Bug');
+INSERT INTO Gym VALUES ('Cyllage','Grant','Rock','Cliff');
+INSERT INTO Gym VALUES ('Shalour','Korrina','Fighting','Rumble');
+INSERT INTO Gym VALUES ('Coumarine','Ramos','Grass','Plant');
+INSERT INTO Gym VALUES ('Lumiose','Clemont','Electric','Voltage');
+INSERT INTO Gym VALUES ('Laverre','Valerie','Fairy','Fairy');
+INSERT INTO Gym VALUES ('Anistar','Olympia','Psychic','Psychic');
+INSERT INTO Gym VALUES ('Snowbelle','Wulfric','Ice','Iceberg');
+INSERT INTO Gym VALUES ('Mikan','Island','Water','Coral-Eye');
+INSERT INTO Gym VALUES ('Navel','Danny','Rock','Sea Ruby');
+INSERT INTO Gym VALUES ('Trovita','Rudy','Grass','Spike Shell');
+INSERT INTO Gym VALUES ('Kumquat','Luana','Psychic','Jade Star');
+INSERT INTO Gym VALUES ('Pummelo','Drake','Dragon','Winners Trophy');
+
+
+
+
+
+
+
