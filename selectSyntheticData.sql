911f5c967a103a584288d38a88c764891eb19ed4
Mason1993 <zhoumuqing@gmail.com>
2015-07-26 18:54:00 -0500

    selectSyntheticData.sql

---
+++ selectSyntheticData.sql
@@ -0,0 +1,16 @@
+# selectSyntheticData.sql
+
+SELECT * FROM each table_name
+SELECT * FROM Appears_In;
+SELECT * FROM Attack;
+SELECT * FROM Badges;
+SELECT * FROM Evolution;
+SELECT * FROM Gym;
+SELECT * FROM Learn;
+SELECT * FROM Owns;
+SELECT * FROM Performs;
+SELECT * FROM Pokemon;
+SELECT * FROM PokemonType;
+SELECT * FROM Statistics;
+SELECT * FROM Trainer;
+SELECT * FROM Weaknesses;
