fa73fd4910620c2181126ae84d6172bc732585e5
jove28 <jove_28@tamu.edu>
2015-07-26 20:25:43 -0500

    Create Owns.sql

---
+++ Owns.sql
@@ -0,0 +1,165 @@
+
+INSERT INTO Pokemon VALUES( 001,'Bulbasaur','B','Seed',False,'Overgrow');
+INSERT INTO Pokemon VALUES( 002,'Ivysaur','B','Seed',True,'Overgrow');
+INSERT INTO Pokemon VALUES( 003,'Venusaur','B','Seed',True,'Overgrow');
+INSERT INTO Pokemon VALUES( 004,'Charmander','B','Lizard',False,'Blaze');
+INSERT INTO Pokemon VALUES( 005,'Charmeleon','B','Flame',True,'Blaze');
+INSERT INTO Pokemon VALUES( 006,'Charizard','B','Flame',True,'Blaze');
+INSERT INTO Pokemon VALUES( 007,'Squirtle','B','Tiny Turtle',False,'Torrent');
+INSERT INTO Pokemon VALUES( 008,'Wartortle','B','Turtle',True,'Torrent');
+INSERT INTO Pokemon VALUES( 009,'Blastoise','B','Shellfish',True,'Torrent');
+INSERT INTO Pokemon VALUES( 010,'Caterpie','B','Worm',False,'Shield Dust');
+
+INSERT INTO Pokemon VALUES( 011,'Metapod','B','Cocoon',True,'Shed Skin');
+INSERT INTO Pokemon VALUES( 012,'Butterfree','B','Butterfly',True,'Compound Eyes');
+INSERT INTO Pokemon VALUES( 013,'Weedle','B','Hairy Bug',False,'Shield Dust');
+INSERT INTO Pokemon VALUES( 014,'Kakuna','B','Cocoon',True,'Shed Skin');
+INSERT INTO Pokemon VALUES( 015,'Beedrill','B','Poison Bee',True,'Swarm');
+INSERT INTO Pokemon VALUES( 016,'Pidgey','B','Tiny Bird',False,'Keen Eye');
+INSERT INTO Pokemon VALUES( 017,'Pidgeotto','B','Bird',True,'Keen Eye');
+INSERT INTO Pokemon VALUES( 018,'Pidgeot','B','Bird',True,'Keen');
+INSERT INTO Pokemon VALUES( 019,'Rattata','B','Mouse',False,'Run Away');
+INSERT INTO Pokemon VALUES( 020,'Raticate','B','Mouse',True,'Run Away');
+
+INSERT INTO Pokemon VALUES( 021,'Spearow','B','Tiny Bird',False,'Keen Eye');
+INSERT INTO Pokemon VALUES( 022,'Fearow','B','Beak',True,'Keen Eye');
+INSERT INTO Pokemon VALUES( 023,'Ekans','B','Snake',False,'Shed Skin');
+INSERT INTO Pokemon VALUES( 025,'Pikachu','B','Mouse',True,'Static');
+INSERT INTO Pokemon VALUES( 026,'Raichu','B','Mouse',True,'Static');
+INSERT INTO Pokemon VALUES( 027,'Sandshrew','B','Mouse',False,'Sand Veil');
+INSERT INTO Pokemon VALUES( 028,'Sandslash','B','Mouse',True,'Sand Veil');
+INSERT INTO Pokemon VALUES( 029,'Nidoran','F','Poison Pin',False,'Poison Point');
+INSERT INTO Pokemon VALUES( 030,'Nidorana','F','Poison Pin',True,'Poison Point');
+
+INSERT INTO Pokemon VALUES( 031,'Nidoqueen','F','Drill',True,'Rivalry');
+INSERT INTO Pokemon VALUES( 032,'Nidoran','M','Poison Pin',False,'Poison Point');
+INSERT INTO Pokemon VALUES( 033,'Nidorano','M','Poison Pin',True,'Poison Point');
+INSERT INTO Pokemon VALUES( 034,'Nidoking','M','Drill',True,'Rivalry');
+INSERT INTO Pokemon VALUES( 035,'Clefairy','B','Fairy',True,'Cute Charm');
+INSERT INTO Pokemon VALUES( 036,'Clefable','B','Fairy',True,'Magic Guard');
+INSERT INTO Pokemon VALUES( 037,'Vulpix','B','Fox',False,'Flash Fire');
+INSERT INTO Pokemon VALUES( 038,'Ninetails','B','Fox',True,'Flash Fire');
+INSERT INTO Pokemon VALUES( 039,'Jigglypuff','B','Balloon',True,'Competitive');
+INSERT INTO Pokemon VALUES( 040,'Wigglytuff','B','Balloon',True,'Competitive');
+
+INSERT INTO Pokemon VALUES( 041,'Zubat','B','Bat',False,'Inner Focus');
+INSERT INTO Pokemon VALUES( 042,'Golbat','B','Bat',True,'Inner Focus');
+INSERT INTO Pokemon VALUES( 043,'Oddish','B','Weed',False,'Chlorophyll');
+INSERT INTO Pokemon VALUES( 044,'Gloom','B','Weed',True,'Chlorophyll');
+INSERT INTO Pokemon VALUES( 045,'Vileplume','B','Flower',True,'Chlorophyll');
+INSERT INTO Pokemon VALUES( 046,'Paras','B','Mushroom',False,'Effect Spore');
+INSERT INTO Pokemon VALUES( 047,'Parasect','B','Mushroom',True,'Dry Skin');
+INSERT INTO Pokemon VALUES( 048,'Venonat','B','Insect',False,'Effect Spore');
+INSERT INTO Pokemon VALUES( 049,'Venomoth','B','Poisin Moth',True,'Sheld Dust');
+INSERT INTO Pokemon VALUES( 050,'Diglett','B','Mole',False,'Sand Veil');
+
+INSERT INTO Pokemon VALUES( 051,'Dugtrio','B','Mole',True,'Arena Trap');
+INSERT INTO Pokemon VALUES( 052,'Meowth','B','Scrath Cat',False,'Pick Up');
+INSERT INTO Pokemon VALUES( 053,'Persian','B','Classay Cat',True,'Limber');
+INSERT INTO Pokemon VALUES( 054,'Psyduck','B','Duck',False,'Damp');
+INSERT INTO Pokemon VALUES( 055,'Golduck','B','Duck',True,'Cloud Nine');
+INSERT INTO Pokemon VALUES( 056,'Makey','B','Pig Monkey',False,'Vital Spirit');
+INSERT INTO Pokemon VALUES( 057,'Primeape','B','Pig Monkey',True,'Anger Point');
+INSERT INTO Pokemon VALUES( 058,'Growlithe','B','Puppy',False,'Intimidate');
+INSERT INTO Pokemon VALUES( 059,'Arcanine','B','Legendary',True,'Flash Fire');
+INSERT INTO Pokemon VALUES( 060,'Poliwag','B','Tadpole',False,'Damp');
+
+INSERT INTO Pokemon VALUES( 061,'Poliwhirl','B','Tadpole',True,'Damp');
+INSERT INTO Pokemon VALUES( 062,'Poliwrath','B','Tadpole',True,'Water Absorb');
+INSERT INTO Pokemon VALUES( 063,'Abra','B','Psi',False,'Inner Focus');
+INSERT INTO Pokemon VALUES( 064,'Kadabra','B','Psi',True,'Inner Focus');
+INSERT INTO Pokemon VALUES( 065,'Alakazam','B','Psi',True,'Synchronize');
+INSERT INTO Pokemon VALUES( 066,'Machop','B','Superpower',False,'Guts');
+INSERT INTO Pokemon VALUES( 067,'Machoke','B','Superpower',True,'No Guard');
+INSERT INTO Pokemon VALUES( 068,'Machamo','B','Superpower',True,'No Guard');
+INSERT INTO Pokemon VALUES( 069,'Bellsprout','B','Flower',False,'Chlorophyll');
+INSERT INTO Pokemon VALUES( 070,'Weepinbell','B','Flycatcher',True,'Chlorophyll');
+
+INSERT INTO Pokemon VALUES( 071,'Victreebel','B','Flycatcher',True,'Chlorophyll');
+INSERT INTO Pokemon VALUES( 072,'Tentacool','B','Jellyfish',False,'Clear Body');
+INSERT INTO Pokemon VALUES( 073,'Tentacruel','B','Jellyfish',True,'Liquid Ooze');
+INSERT INTO Pokemon VALUES( 074,'Geodude','B','Rock',False,'Rock Head');
+INSERT INTO Pokemon VALUES( 075,'Graveler','B','Rock',True,'Rock Head');
+INSERT INTO Pokemon VALUES( 076,'Golem','B','Rock',True,'Sturdy');
+INSERT INTO Pokemon VALUES( 077,'Ponyta','B','Fire Horse',False,'Run Away');
+INSERT INTO Pokemon VALUES( 078,'Rapidash','B','Fire Horse',True,'Flash Fire');
+INSERT INTO Pokemon VALUES( 079,'Slowpoke','B','Dopey',False,'Oblivious');
+INSERT INTO Pokemon VALUES( 080,'Slowbro','B','Hermit Crab',True,'Own Tempo');
+
+INSERT INTO Pokemon VALUES( 081,'Magnemite','U','Magnet',False,'Sturdy');
+INSERT INTO Pokemon VALUES( 082,'Magneton','U','Magnet',True,'Magnet Pull');
+INSERT INTO Pokemon VALUES( 083,'Farfetchd','B','Wild Duck',False,'Inner Focus');
+INSERT INTO Pokemon VALUES( 084,'Doduo','B','Twin Bird',False,'Run Away');
+INSERT INTO Pokemon VALUES( 085,'Dodrio','B','Triple Bird',True,'Early Bird');
+INSERT INTO Pokemon VALUES( 086,'Seel','B','Sea Lion',False,'Thick Fat');
+INSERT INTO Pokemon VALUES( 087,'Dewgong','B','Sea Lion',True,'Hydration');
+INSERT INTO Pokemon VALUES( 088,'Grimer','B','Sludge',False,'Stench');
+INSERT INTO Pokemon VALUES( 089,'Muk','B','Sludge',True,'Sticky Hold');
+INSERT INTO Pokemon VALUES( 090,'Shellder','B','Bivalve',False,'Shell Armor');
+
+INSERT INTO Pokemon VALUES( 091,'Cloyster','B','Bivalve',True,'Skill Link');
+INSERT INTO Pokemon VALUES( 092,'Gastly','B','Gas',False,'Levitate');
+INSERT INTO Pokemon VALUES( 093,'Haunter','B','Gas',True,'Levitate');
+INSERT INTO Pokemon VALUES( 094,'Gengar','B','Shadow',True,'Levitate');
+INSERT INTO Pokemon VALUES( 095,'Onix','B','Rock Snake',False,'Rock Head');
+INSERT INTO Pokemon VALUES( 096,'Drowzee','B','Hypnosis',False,'Insomnia');
+INSERT INTO Pokemon VALUES( 097,'Hypno','B','Hypnosis',True,'Forewarn');
+INSERT INTO Pokemon VALUES( 098,'Krabby','B','River Crab',False,'Shell Armor');
+INSERT INTO Pokemon VALUES( 099,'Kingler','B','Pincer',True,'Hyper Cutter');
+INSERT INTO Pokemon VALUES( 100,'Voltorn','U','Ball',False,'Static');
+
+INSERT INTO Pokemon VALUES( 101,'Electrode','U','Ball',True,'Soundproof');
+INSERT INTO Pokemon VALUES( 102,'Exeggcute','B','Egg',False,'Chlorophyll');
+INSERT INTO Pokemon VALUES( 103,'Exeggutor','B','Coconut',True,'Chlorophyll');
+INSERT INTO Pokemon VALUES( 104,'Cubone','B','Lonely',False,'Rock Head');
+INSERT INTO Pokemon VALUES( 105,'Marowak','B','Bone Keeper',True,'Lightning Rod');
+INSERT INTO Pokemon VALUES( 106,'Hitmonlee','M','Kicking',True,'Reckless');
+INSERT INTO Pokemon VALUES( 107,'Hitmonchan','M','Punching',True,'Iron Fist');
+INSERT INTO Pokemon VALUES( 108,'Lickitung','B','Licking',False,'Own Tempo');
+INSERT INTO Pokemon VALUES( 109,'Koffing','B','Poison Gas',False,'Levitate');
+INSERT INTO Pokemon VALUES( 110,'Weezing','B','Poison Gas',True,'Levitate');
+
+INSERT INTO Pokemon VALUES( 111,'Rhyhorn','B','Spikes',False,'Rock Head');
+INSERT INTO Pokemon VALUES( 112,'Rhydon','B','Drill',True,'Lightning Rod');
+INSERT INTO Pokemon VALUES( 113,'Chansey','F','Egg',True,'Serence grace');
+INSERT INTO Pokemon VALUES( 114,'Tangela','B','Vine',False,'Leaf Guard');
+INSERT INTO Pokemon VALUES( 115,'Kangaskhan','F','Parent',False,'Scrappy');
+INSERT INTO Pokemon VALUES( 116,'Horsea','B','Dragon',False,'Sniper');
+INSERT INTO Pokemon VALUES( 117,'Seadra','B','Dragon',True,'Poison Point');
+INSERT INTO Pokemon VALUES( 118,'Goldeen','B','Goldfish',False,'Water Veil');
+INSERT INTO Pokemon VALUES( 119,'Seaking','B','Goldfish',True,'Swift Swim');
+
+( 120,'Staryu','U','Star Shape',False,'Nature Cure' );
+( 121,'Starmie','U','Mysterious',True,'Illuminate' );
+( 122,'Mr.Mime','B','Barrier',True,'Filter' );
+( 123,'Scyther','B','Mantis',False,'Technician' );
+( 121,'Satrmie','U','Mysterious',True,'Illuminate' );
+
+124,'Jynx','M','Human Shape', True,'Forewarn'
+125,'Electrabuzz','B','Electric',True,'Static'
+126,'Magmar','B','Spitfire',True,'Flame Body'
+127,'Pinsir','B','Stag Beetle',False,'Nature Cure'
+128,'Tauros','M','Wild Bull',False,'Anger Point'
+129,'Magikarp','B','Fish',False,'Swift Swim'
+130,'Gyarados','B','Atrocious',True,'Intimidate'
+131,'Lapras','B','Transport',False,'Shell Armor'
+132,'Ditto','U','Transform',False,'Limber'
+133,'Eevee','B','Evolution',False,'Adaptability'
+134,'Vaporeon','B','Bubble Jet',True,'Water Absorb'
+135,'Joltean','B','Lightning',True,'Volt Absorb'
+136,'Flareon','B','Flame',True,'Flash Fire'
+137,'Porygon','U','Virtual',False,'Download'
+138,'Omanyte','B','Spiral',False,'Shell Armor'
+139,'Omastar','B','Spiral',True,'Swift Armor'
+140,'Kabuto','B','Shellfish',True,'Swift Swim'
+141,'Kabutops','B','Shellfish',True,'Battle Armor'
+142,'Aerodactyl','B','Fossil',False,'Pressure'
+143,'Snorlax','B','Sleeping',True,'Thick Fat'
+144,'Articuno','U','Freeze',False,'Pressure'
+145,'Zapdos','U','Electric',False,'Pressure'
+146,'Moltres','U','Flame',False,'Pressure'
+147,'Dratini','B','Dragon',False,'Shed Skin'
+148,'Dragonair','B','Dragon',True,'Shed Skin'
+149,'Dragonite','B','Dragon',True,'Inner Focus'
+150,'Mewtwo','U','Genetic',False,'Pressure'
+151,'Mew','U','New Species',False,'Synchronize'
