e0cc0a1bdbebe9f1a555cf70e709a076b74f5b2a
Mason1993 <zhoumuqing@gmail.com>
2015-08-02 16:09:04 -0500

    part4-muqing done

---
+++ SQL-Scripts/dataRetrievalCommand-Muqing.sql
@@ -0,0 +1,13 @@
+# Muqing's SQL data retreival & modification commands, general command and aggregate command
+
+SELECT * FROM Learn;
+SELECT COUNT(*) AS Total
+FROM Learn;
+
+# modify data
+DELETE FROM Learn
+WHERE attackName = "Toxic";
+
+SELECT * FROM Learn;
+SELECT COUNT(*) AS Total
+FROM Learn;
