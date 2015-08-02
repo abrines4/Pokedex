62ac13d26cf6b156d1414fd52e12825329f45f87
abrines4 <oscarcat@tamu.edu>
2015-08-02 17:38:41 -0500

    Update dataRetrievalCommand-Muqing.sql

--- SQL-Scripts/dataRetrievalCommand-Muqing.sql
+++ SQL-Scripts/dataRetrievalCommand-Muqing.sql
@@ -11,3 +11,8 @@ WHERE attackName = "Toxic";
 SELECT * FROM Learn;
 SELECT COUNT(*) AS Total
 FROM Learn;
+
+SELECT Appears_In.pokemonID, Weaknesses.weakness 
+FROM Weaknesses, Appears_In 
+WHERE (Appears_In.gymName = 'Pewter' OR weakness = 'Ice') 
+              AND Appears_In.pokemonID = Weaknesses.pokemonID;
