cea7b825b026497834ed01868dcfcd5e1db14d92
jove28 <jove_28@tamu.edu>
2015-08-02 20:05:45 -0500

    added modifications and queries

--- SQL-Scripts/dataRetrievalCommand-Muqing.sql
+++ SQL-Scripts/dataRetrievalCommand-Muqing.sql
@@ -26,3 +26,32 @@ UNION
 SELECT pokemonID 
 FROM Weaknesses
 WHERE weakness = 'Fairy';
+
+# Ivins' Queries;
+SELECT p.pokemonId, p.pokemonName, p.ability, s.defense, s.attack, s.hp, s.specialAttack, s.specialDefense, t.pokemonType
+FROM Pokemon AS p
+JOIN Statistics AS s ON p.pokemonID = s.pokemonID 
+JOIN PokemonType AS t ON t.pokemonID = p.pokemonID;
+
+SELECT pokemonID, pokemonName, category, ability 
+FROM Pokemon
+GROUP BY ability, category;
+
+#Ivins' modifications 
+Update PokemonType
+set pokemonType = 'Shenanigans'
+where pokemonType = 'Fairy' ;
+
+INSERT INTO Pokemon (pokemonID, pokemonName, gender, category, evolved, ability) VALUES (386, 'Deoxys', 'U', 'DNA Pokemon', FALSE , 'Pressure');
+INSERT INTO PokemonType( pokemonID, PokemonType) VALUES ( 386 , 'Psychic');
+INSERT INTO Statistics ( pokemonID, defense, attack, speed, hp, specialAttack, specialDefense ) VALUES (386, 2, 8, 8,2,7,2);
+INSERT INTO Weaknesses (pokemonID,weakness) VALUES (386 , 'Bug');
+INSERT INTO Weaknesses (pokemonID,weakness) VALUES (386 , 'Ghost');
+INSERT INTO Weaknesses (pokemonID,weakness) VALUES (386 , 'Dark');
+INSERT INTO Learn (attackName, pokemonName) VALUES ('Rain Dance', 'Deoxys');
+INSERT INTO Learn (attackName, pokemonName) VALUES ('Thunder', 'Deoxys');
+INSERT INTO Learn (attackName, pokemonName) VALUES ('Solar Beam', 'Deoxys');
+INSERT INTO Performs (pokemonID, attackName) VALUES (386, 'Psychic');
+INSERT INTO Performs (pokemonID, attackName) VALUES (386, 'Extreme Speed');
+INSERT INTO Performs (pokemonID, attackName) VALUES (386, 'Night Shade');
+
