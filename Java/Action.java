658623950e34321c7b3f50ebcc4cf3676c790c3d
abrines4 <oscarcat@tamu.edu>
2015-08-06 16:54:13 -0500

    Create Action.java

---
+++ Java/Action.java
@@ -0,0 +1,28 @@
+public class Action {
+	private Statement s;
+	
+	public Action(Statement conn) {s = conn;}
+	
+	public String deletePokemon(){
+    try{
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the ID number of the Poekmon you would like to delete: ");
+		int pokemonID = scan.nextInt();
+		
+		s.executeUpdate("DELETE FROM Pokemon WHERE "+ pokemonID +" =  pokemonID ");
+		s.executeUpdate("DELETE FROM Owns WHERE "+ pokemonID +" =  pokemonID ");
+		s.executeUpdate("DELETE FROM Performs WHERE "+ pokemonID +" =  pokemonID ");
+		s.executeUpdate("DELETE FROM Appears_In WHERE "+ pokemonID +" =  pokemonID ");
+		s.executeUpdate("DELETE FROM PokemonType WHERE "+ pokemonID +" =  pokemonID ");
+		s.executeUpdate("DELETE FROM Weaknesses WHERE "+ pokemonID +" =  pokemonID ");
+		s.executeUpdate("DELETE FROM Evolution WHERE "+ pokemonID +" =  pokemonID ");
+		s.executeUpdate("DELETE FROM Statistics WHERE "+ pokemonID +" =  pokemonID ");
+		return "Successful deletion";
+		}
+		catch(SQLException e){
+			System.err.println ("Error message: " + e.getMessage ());
+       			System.err.println ("Error number: " + e.getErrorCode ());
+			return "Unsuccessful deletion";
+		}
+	}
+}
