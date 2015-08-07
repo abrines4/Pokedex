ed7a0005a1d429926752370010dfb917f1bebb6d
LandonPrewitt <landonprewitt@gmail.com>
2015-08-07 18:41:38 -0500

    Create refreshData.sql
    shortcut sourcing file

---
+++ SQL-Scripts/refreshData.sql
@@ -0,0 +1,13 @@
+source createTable.sql;
+source insertAppearsIn.sql;
+source insertBadges.sql;
+source insertGym.sql;
+source insertOwns.sql;
+source insertPerforms.sql;
+source insertPokemon.sql;
+source insertPokemonType.sql;
+source insertStatistics.sql;
+source insertTrainers.sql;
+source insertWeaknesses.sql;
+
+# missing Attack, Evolution, and Learn.
