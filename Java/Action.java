35d066c1ec44275ea123a193fcecd71e546f77b1
abrines4 <oscarcat@tamu.edu>
2015-08-07 16:32:25 -0500

    Update Action.java

--- Java/Action.java
+++ Java/Action.java
@@ -28,4 +28,24 @@ public class Action {
 			return "Unsuccessful deletion " + pokemonID;
 		}
 	}
+	
+	public String deleteAttack(){
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the name of the Attack you would like to delete: ");
+		String attack = scan.nextLine();
+		try{
+		System.out.println(attack);
+		s.executeUpdate("DELETE FROM Attack WHERE '" + attack +"' = attackName");
+		s.executeUpdate("DELETE FROM Performs WHERE '" + attack +"' = attackName");
+		s.executeUpdate("DELETE FROM Learn WHERE '" + attack +"' = attackName");
+		
+		return "Successful deletion of attack: " + attack;
+		}
+		catch(SQLException e){
+			System.err.println ("Error message: " + e.getMessage ());
+			System.err.println ("Error number: " + e.getErrorCode ());
+			return "Unsuccessful deletion of attack: " + attack;
+		}
+	}
 }
